4c7c0d1fbddfb268ea88c71cfa46f5042dd6547c
matt-kukla <matt.kukla@yandex.com>
2021-09-17 12:50:18 -0400

    expand bnp functionality

--- src/bnp.ml
+++ src/bnp.ml
@@ -2,10 +2,16 @@ type belnap = T | F | N | B
 
 type bnp_expr = 
     | Val of belnap
+    | Not of bnp_expr
+    | Cnf of bnp_expr
     | And of bnp_expr * bnp_expr
     | Or of bnp_expr * bnp_expr
-    | Not of bnp_expr
     | Impl of bnp_expr * bnp_expr
+    | Impl_CMI of bnp_expr * bnp_expr
+    | Impl_BN of bnp_expr * bnp_expr
+    | Impl_ST of bnp_expr * bnp_expr
+    | Cns of bnp_expr * bnp_expr
+    | Gul of bnp_expr * bnp_expr
 
 let not_bnp x =
 match x with
@@ -13,6 +19,12 @@ match x with
     | F -> T
     | N -> N
     | B -> B
+
+let conf x =
+match x with 
+    | (T|F) -> x
+    | B -> N
+    | N -> B
 
 let and_bnp x y =
 match x, y with
@@ -36,11 +48,50 @@ match x, y with
     | T, B -> T
     | T, (F|N) -> F
     | B, (F|N) -> F
-    
+
+let implic_cmi x y = 
+match x, y with
+    | _, T | F, _ | N, _ -> T
+    | T, z -> z
+    | B, z -> z
+
+let implic_bn x y = 
+match x, y with
+    | _, T | F, _  | B, B | N, N -> T
+    | (T|B), F | (T|B), N | N, F | (T|N), B -> F
+
+let implic_st x y =
+match x, y with
+    | _, T | F, _ | N, N -> T
+    | T, F | T, B | B, F -> F
+    | (T|B), N | N, (F|B) -> N
+    | B, B -> B
+
+let cns x y =
+match x, y with
+    | T, (T|B) | B, T -> T
+    | F, (F|B) | B, F -> F
+    | F, (T|N) | T, (F|N) -> N
+    | B, B -> B
+    | N, _ | _, N-> N
+
+let gull x y =
+match x, y with
+    | T, N | N, T | T, T -> T
+    | F, F | F, N | N, F -> F
+    | N, N -> N
+    | B, _ | _, B | T, F | F, T -> B
+
 let rec eval_bnp e = 
 match e with
     | Val(x) -> x
+    | Not(x) -> not_bnp (eval_bnp x)
+    | Cnf(x) -> conf (eval_bnp x) 
     | And(l, r) -> and_bnp (eval_bnp l) (eval_bnp r)
     | Or(l, r) -> or_bnp (eval_bnp l) (eval_bnp r)
-    | Not(x) -> not_bnp (eval_bnp  x)
-    | Impl(l, r) -> implic(eval_bnp l) (eval_bnp r) 
+    | Impl(l, r) -> implic(eval_bnp l) (eval_bnp r)
+    | Impl_CMI(l, r) -> implic_cmi(eval_bnp l) (eval_bnp r)
+    | Impl_BN(l, r) -> implic_bn (eval_bnp l) (eval_bnp r)
+    | Impl_ST(l, r) -> implic_st (eval_bnp l) (eval_bnp r)
+    | Cns(l, r) -> cns (eval_bnp l) (eval_bnp r)
+    | Gul(l, r) -> gull (eval_bnp l) (eval_bnp r)
--- src/bnp.mli
+++ src/bnp.mli
@@ -6,15 +6,24 @@
 type belnap = T | F | N | B
 
 type bnp_expr =
-    Val of belnap
+    |Val of belnap
+    | Not of bnp_expr
+    | Cnf of bnp_expr
     | And of bnp_expr * bnp_expr
     | Or of bnp_expr * bnp_expr
-    | Not of bnp_expr
     | Impl of bnp_expr * bnp_expr
-    
+    | Impl_CMI of bnp_expr * bnp_expr
+    | Impl_BN of bnp_expr * bnp_expr
+    | Impl_ST of bnp_expr * bnp_expr
+    | Cns of bnp_expr * bnp_expr
+    | Gul of bnp_expr * bnp_expr
+
 (** Negation *)
 val not_bnp : belnap -> belnap
-    
+
+(** Conflation *)
+val conf : belnap -> belnap
+
 (** Conjunction *)
 val and_bnp : belnap -> belnap -> belnap
     
@@ -23,6 +32,21 @@ val or_bnp : belnap -> belnap -> belnap
 
 (** Truth-preserving implication  *)
 val implic : belnap -> belnap -> belnap
-    
+
+(** Material implication *)
+val implic_cmi : belnap -> belnap -> belnap
+
+(** Belnap implication *)
+val implic_bn : belnap -> belnap -> belnap
+
+(** Strong implication *)
+val implic_st : belnap -> belnap -> belnap
+
+(** Consensus *)
+val cns : belnap -> belnap -> belnap
+
+(** Gullibility *)
+val gull : belnap -> belnap -> belnap
+
 (** Evaluate formula *)
 val eval_bnp : bnp_expr -> belnap
